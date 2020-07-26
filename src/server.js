e87c85c7c65d74f5f4026d6b4bbec5457a465938
Gonzalo Andrade <gonzalo.andrade@gmailcom>
2020-07-25 23:16:26 -0400

    Add Route basic to server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = new express();
 const port = process.env.PORT || 4000;
 const version = require('../package.json').version;
 
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
 const server = app.listen(port, function() {
     console.log(`[API-PRODUCTS] HTTP-ADAPTER server started on port: ${port} - version: ${version}`);
 });
