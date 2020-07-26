f53cb40d4e5fd9af80cab639bda0cce5da249ecc
Gonzalo Andrade <gonzalo.andrade@gmailcom>
2020-07-25 23:06:39 -0400

    First commit from Linux Terminal

---
+++ src/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = new express();
+
+const port = process.env.PORT || 4000;
+const version = require('../package.json').version;
+
+const server = app.listen(port, function() {
+    console.log(`[API-PRODUCTS] HTTP-ADAPTER server started on port: ${port} - version: ${version}`);
+});
+
+module.exports = server;
